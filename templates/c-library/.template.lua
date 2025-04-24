464e673dbdf6585e3f5c08b59bdf0a2fa10df065
TarekSaeed0 <naminode2020@gmail.com>
2025-04-24 16:25:11 +0200

    .

--- templates/c-library/.template.lua
+++ templates/c-library/.template.lua
@@ -1,3 +1,3 @@
 return os.execute("git init") == 0
-	and os.execute("git submodule add https://github.com/jothepro/doxygen-awesome-css.git") == 0
+	and os.execute("git -C doc submodule add https://github.com/jothepro/doxygen-awesome-css.git") == 0
 	and os.execute("cmake -S . -B build -DUNIT_TESTING=1") == 0

ab7a4fba662361644041f1ec32858d05b25ceaaf
TarekSaeed0 <naminode2020@gmail.com>
2026-01-02 10:31:52 +0200

    feat: add paging to buffer list

--- lua/config/plugins/extras/ui/heirline/tabline/components/buffers.lua
+++ lua/config/plugins/extras/ui/heirline/tabline/components/buffers.lua
@@ -1,278 +1,428 @@
 local heirline_utils = require("heirline.utils")
 
-local buffers = {
+local icon = {
+	init = function(self)
+		local name = vim.api.nvim_buf_get_name(self.buffer)
+		local stat = vim.uv.fs_stat(name)
+		if stat and stat.type == "directory" then
+			self.icon, self.color = "", require("nvim-web-devicons").get_default_icon().color
+		else
+			self.icon, self.color =
+				require("nvim-web-devicons").get_icon_color(vim.fn.fnamemodify(name, ":t"), vim.fn.fnamemodify(name, ":e"))
+			if self.icon == nil then
+				self.icon, self.color = "", require("nvim-web-devicons").get_default_icon().color
+			end
+		end
+	end,
 	{
-		static = {
-			buffer = {
+		provider = function(self)
+			return " " .. (self.icon or "")
+		end,
+		hl = function(self)
+			return { fg = self.color, bold = false }
+		end,
+	},
+	update = { "BufAdd", "BufEnter", "BufLeave", "FocusGained", "FocusLost" },
+}
+
+local path = {
+	init = function(self)
+		local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(self.buffer), ":~:."):gsub("%%", "%%%%")
+
+		local separator = package.config:sub(1, 1)
+		local ellipsis = "…"
+
+		local components = vim.split(path, separator)
+
+		local child = { flexible = 50 }
+
+		child[1] = {
+			{ provider = " " },
+			{
+				provider = table.concat(components, separator, 1, #components - 1) .. (#components > 1 and separator or ""),
+				hl = { fg = heirline_utils.get_highlight("TabLine").fg, bold = false },
+			},
+			{
+				provider = components[#components],
+			},
+			{ provider = " " },
+		}
+		for i = 2, #components do
+			child[i] = {
+				{ provider = " " },
 				{
-					provider = "",
-					hl = function(self)
-						return {
-							fg = heirline_utils.get_highlight(self.is_active and "TabLineSel" or "TabLine").bg,
-							bg = heirline_utils.get_highlight("TabLineFill").bg,
-						}
-					end,
+					provider = ellipsis
+						.. separator
+						.. table.concat(components, separator, i, #components - 1)
+						.. (#components > i and separator or ""),
+					hl = { fg = heirline_utils.get_highlight("TabLine").fg, bold = false },
 				},
 				{
-					init = function(self)
-						local name = vim.api.nvim_buf_get_name(self.buffer)
-						local stat = vim.uv.fs_stat(name)
-						if stat and stat.type == "directory" then
-							self.icon, self.color = "", require("nvim-web-devicons").get_default_icon().color
-						else
-							self.icon, self.color = require("nvim-web-devicons").get_icon_color(
-								vim.fn.fnamemodify(name, ":t"),
-								vim.fn.fnamemodify(name, ":e")
-							)
-							if self.icon == nil then
-								self.icon, self.color = "", require("nvim-web-devicons").get_default_icon().color
-							end
-						end
-					end,
-					{
-						provider = function(self)
-							return " " .. self.icon
-						end,
-						hl = function(self)
-							return { fg = self.color, bold = false }
-						end,
-					},
-					update = { "BufAdd", "BufEnter", "BufLeave", "FocusGained", "FocusLost" },
+					provider = components[#components],
 				},
-				{
-					init = function(self)
-						local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(self.buffer), ":~:."):gsub("%%", "%%%%")
+				{ provider = " " },
+			}
+		end
+		child[#components + 1] = nil
 
-						local separator = package.config:sub(1, 1)
-						local ellipsis = "…"
+		self[1] = self:new(child)
+	end,
+	update = { "BufAdd", "BufEnter", "BufLeave", "DirChanged", "VimResized", "FocusGained", "FocusLost" },
+}
 
-						local components = vim.split(path, separator)
+local diagnostics = {
+	flexible = 60,
+	init = function(self)
+		self.error_count = #vim.diagnostic.get(self.buffer, { severity = vim.diagnostic.severity.ERROR })
+		self.warning_count = #vim.diagnostic.get(self.buffer, { severity = vim.diagnostic.severity.WARN })
+		self.information_count = #vim.diagnostic.get(self.buffer, { severity = vim.diagnostic.severity.INFO })
+		self.hint_count = #vim.diagnostic.get(self.buffer, { severity = vim.diagnostic.severity.HINT })
+	end,
+	condition = function(self)
+		return not self.is_active and #vim.diagnostic.get(self.buffer) ~= 0
+	end,
+	update = { "DiagnosticChanged", "BufEnter", "BufLeave" },
+}
 
-						local child = { flexible = 50 }
+local diagnostics_components = {
+	{
+		provider = function(self)
+			return " " .. self.error_count .. " "
+		end,
+		hl = "DiagnosticError",
+		condition = function(self)
+			return self.error_count ~= 0
+		end,
+	},
+	{
+		provider = function(self)
+			return " " .. self.warning_count .. " "
+		end,
+		hl = "DiagnosticWarn",
+		condition = function(self)
+			return self.warning_count ~= 0
+		end,
+	},
+	{
+		provider = function(self)
+			return " " .. self.information_count .. " "
+		end,
+		hl = "DiagnosticInfo",
+		condition = function(self)
+			return self.information_count ~= 0
+		end,
+	},
+	{
+		provider = function(self)
+			return "󰌵 " .. self.hint_count .. " "
+		end,
+		hl = "DiagnosticHint",
+		condition = function(self)
+			return self.hint_count ~= 0
+		end,
+	},
+}
 
-						child[1] = {
-							{ provider = " " },
-							{
-								provider = table.concat(components, separator, 1, #components - 1)
-									.. (#components > 1 and separator or ""),
-								hl = { fg = heirline_utils.get_highlight("TabLine").fg, bold = false },
-							},
-							{
-								provider = components[#components],
-							},
-							{ provider = " " },
-						}
-						for i = 2, #components do
-							child[i] = {
-								{ provider = " " },
-								{
-									provider = ellipsis
-										.. separator
-										.. table.concat(components, separator, i, #components - 1)
-										.. (#components > i and separator or ""),
-									hl = { fg = heirline_utils.get_highlight("TabLine").fg, bold = false },
-								},
-								{
-									provider = components[#components],
-								},
-								{ provider = " " },
-							}
-						end
-						child[#components + 1] = nil
+diagnostics[#diagnostics_components + 1] = { provider = "" }
+for i = #diagnostics_components, 1, -1 do
+	diagnostics[i] = { diagnostics_components[i], (table.unpack or unpack)(diagnostics[i + 1]) }
+end
 
-						self[1] = self:new(child, 2)
-					end,
-					update = { "BufAdd", "BufEnter", "BufLeave", "DirChanged", "VimResized", "FocusGained", "FocusLost" },
-				},
-				(function()
-					local diagnostics = {
-						flexible = 60,
-						init = function(self)
-							self.error_count = #vim.diagnostic.get(self.buffer, { severity = vim.diagnostic.severity.ERROR })
-							self.warning_count = #vim.diagnostic.get(self.buffer, { severity = vim.diagnostic.severity.WARN })
-							self.information_count = #vim.diagnostic.get(self.buffer, { severity = vim.diagnostic.severity.INFO })
-							self.hint_count = #vim.diagnostic.get(self.buffer, { severity = vim.diagnostic.severity.HINT })
-						end,
-						condition = function(self)
-							return not self.is_active and #vim.diagnostic.get(self.buffer) ~= 0
-						end,
-						update = { "DiagnosticChanged", "BufEnter", "BufLeave" },
-					}
-
-					local components = {
-						{
-							provider = function(self)
-								return " " .. self.error_count .. " "
-							end,
-							hl = "DiagnosticError",
-							condition = function(self)
-								return self.error_count ~= 0
-							end,
-						},
-						{
-							provider = function(self)
-								return " " .. self.warning_count .. " "
-							end,
-							hl = "DiagnosticWarn",
-							condition = function(self)
-								return self.warning_count ~= 0
-							end,
-						},
-						{
-							provider = function(self)
-								return " " .. self.information_count .. " "
-							end,
-							hl = "DiagnosticInfo",
-							condition = function(self)
-								return self.information_count ~= 0
-							end,
-						},
-						{
-							provider = function(self)
-								return "󰌵 " .. self.hint_count .. " "
-							end,
-							hl = "DiagnosticHint",
-							condition = function(self)
-								return self.hint_count ~= 0
-							end,
-						},
-					}
-
-					diagnostics[#components + 1] = { provider = "" }
-					for i = #components, 1, -1 do
-						diagnostics[i] = { components[i], (table.unpack or unpack)(diagnostics[i + 1]) }
+local modified = {
+	{
+		provider = "●",
+		hl = {
+			fg = "green",
+			bold = false,
+		},
+		on_click = {
+			callback = function(_, buffer)
+				vim.schedule(function()
+					if vim.api.nvim_buf_is_valid(buffer) then
+						vim.api.nvim_buf_call(buffer, vim.cmd.write)
 					end
+					vim.cmd.redrawtabline()
+				end)
+			end,
+			minwid = function(self)
+				return self.buffer
+			end,
+			name = "heirline_buffer_write_callback",
+		},
+	},
+	{ provider = " " },
+	condition = function(self)
+		return vim.api.nvim_get_option_value("modified", { buf = self.buffer })
+	end,
+	update = { "BufModifiedSet", "BufEnter", "BufLeave" },
+}
 
-					return diagnostics
-				end)(),
-				{
-					{
-						provider = "●",
-						hl = {
-							fg = "green",
-							bold = false,
-						},
-						on_click = {
-							callback = function(_, buffer)
-								vim.schedule(function()
-									if vim.api.nvim_buf_is_valid(buffer) then
-										vim.api.nvim_buf_call(buffer, vim.cmd.write)
-									end
-									vim.cmd.redrawtabline()
-								end)
-							end,
-							minwid = function(self)
-								return self.buffer
-							end,
-							name = "heirline_buffer_write_callback",
-						},
-					},
-					{ provider = " " },
-					condition = function(self)
-						return vim.api.nvim_get_option_value("modified", { buf = self.buffer })
-					end,
-					update = { "BufModifiedSet", "BufEnter", "BufLeave" },
-				},
-				{
-					provider = " ",
-					hl = {
-						fg = "yellow",
-						bold = false,
-					},
-					condition = function(self)
-						return not vim.api.nvim_get_option_value("modifiable", { buf = self.buffer })
-							or vim.api.nvim_get_option_value("readonly", { buf = self.buffer })
-					end,
-				},
-				{
-					{
-						provider = "",
-						on_click = {
-							callback = function(_, buffer)
-								vim.schedule(function()
-									if vim.api.nvim_buf_is_valid(buffer) then
-										Snacks.bufdelete(buffer)
-									end
-									vim.cmd.redrawtabline()
-								end)
-							end,
-							minwid = function(self)
-								return self.buffer
-							end,
-							name = "heirline_buffer_close_callback",
-						},
-					},
-					{ provider = " " },
-					condition = function(self)
-						return not vim.api.nvim_get_option_value("modified", { buf = self.buffer })
-					end,
-					update = { "BufModifiedSet", "BufEnter", "BufLeave", "FocusGained", "FocusLost" },
-				},
-				{
-					provider = "",
-					hl = function(self)
-						return {
-							fg = heirline_utils.get_highlight(self.is_active and "TabLineSel" or "TabLine").bg,
-							bg = heirline_utils.get_highlight("TabLineFill").bg,
-						}
-					end,
-				},
-				hl = function(self)
-					return self.is_active and "TabLineSel" or "TabLine"
-				end,
-				on_click = {
-					callback = function(_, buffer, _, button)
-						if button == "l" then
-							vim.api.nvim_win_set_buf(0, buffer)
-						elseif button == "m" then
-							vim.schedule(function()
-								if vim.api.nvim_buf_is_valid(buffer) then
-									Snacks.bufdelete(buffer)
-								end
-								vim.cmd.redrawtabline()
-							end)
-						end
-					end,
-					minwid = function(self)
-						return self.buffer
-					end,
-					name = "heirline_buffer_callback",
-				},
-				update = {
-					"BufEnter",
-					"BufLeave",
-					"BufModifiedSet",
-					"BufWritePost",
-					"DirChanged",
-					"VimResized",
-					"FocusGained",
-					"FocusLost",
-				},
+local readonly = {
+	provider = " ",
+	hl = {
+		fg = "yellow",
+		bold = false,
+	},
+	condition = function(self)
+		return not vim.api.nvim_get_option_value("modifiable", { buf = self.buffer })
+			or vim.api.nvim_get_option_value("readonly", { buf = self.buffer })
+	end,
+}
+
+local close_button = {
+	{
+		provider = "",
+		on_click = {
+			callback = function(_, buffer)
+				vim.schedule(function()
+					if vim.api.nvim_buf_is_valid(buffer) then
+						Snacks.bufdelete(buffer)
+					end
+					vim.cmd.redrawtabline()
+				end)
+			end,
+			minwid = function(self)
+				return self.buffer
+			end,
+			name = "heirline_buffer_close_callback",
+		},
+	},
+	{ provider = " " },
+	condition = function(self)
+		return not vim.api.nvim_get_option_value("modified", { buf = self.buffer })
+	end,
+	update = { "BufModifiedSet", "BufEnter", "BufLeave", "FocusGained", "FocusLost" },
+}
+
+local buffer_template = {
+	{
+		provider = "",
+		hl = function(self)
+			return {
+				fg = heirline_utils.get_highlight(self.is_active and "TabLineSel" or "TabLine").bg,
+				bg = heirline_utils.get_highlight("TabLineFill").bg,
+			}
+		end,
+	},
+	icon,
+	path,
+	diagnostics,
+	modified,
+	readonly,
+	close_button,
+	{
+		provider = "",
+		hl = function(self)
+			return {
+				fg = heirline_utils.get_highlight(self.is_active and "TabLineSel" or "TabLine").bg,
+				bg = heirline_utils.get_highlight("TabLineFill").bg,
+			}
+		end,
+	},
+	hl = function(self)
+		return self.is_active and "TabLineSel" or "TabLine"
+	end,
+	on_click = {
+		callback = function(_, buffer, _, button)
+			if button == "l" then
+				vim.api.nvim_win_set_buf(0, buffer)
+			elseif button == "m" then
+				vim.schedule(function()
+					if vim.api.nvim_buf_is_valid(buffer) then
+						Snacks.bufdelete(buffer)
+					end
+					vim.cmd.redrawtabline()
+				end)
+			end
+		end,
+		minwid = function(self)
+			return self.buffer
+		end,
+		name = "heirline_buffer_callback",
+	},
+	update = {
+		"BufEnter",
+		"BufLeave",
+		"BufModifiedSet",
+		"BufWritePost",
+		"DirChanged",
+		"VimResized",
+		"FocusGained",
+		"FocusLost",
+	},
+}
+
+local next_page_button = {
+	{ provider = " " },
+	{
+		{
+			provider = "",
+			hl = { fg = "accent" },
+		},
+		{
+			provider = "󰜴",
+			hl = {
+				fg = "accent",
+				reverse = true,
+				bold = true,
 			},
 		},
+		{
+			provider = "",
+			hl = { fg = "accent" },
+		},
+		on_click = {
+			callback = function(self)
+				self.shared.page = math.min(self.shared.page_count, self.shared.page + 1)
+				self.shared.page_forced = true
+
+				self.shared.parent:broadcast(function(self)
+					self._win_cache = nil
+				end)
+				vim.cmd.redrawtabline()
+			end,
+			name = "heirline_buffers_next_page_callback",
+		},
+	},
+	condition = function(self)
+		return self.shared.page < self.shared.page_count
+	end,
+}
+
+local previous_page_button = {
+	{
+		{
+			provider = "",
+			hl = { fg = "accent" },
+		},
+		{
+			provider = "󰜱",
+			hl = {
+				fg = "accent",
+				reverse = true,
+				bold = true,
+			},
+		},
+		{
+			provider = "",
+			hl = { fg = "accent" },
+		},
+		on_click = {
+			callback = function(self)
+				self.shared.page = math.max(1, self.shared.page - 1)
+				self.shared.page_forced = true
+
+				self.shared.parent:broadcast(function(self)
+					self._win_cache = nil
+				end)
+				vim.cmd.redrawtabline()
+			end,
+			name = "heirline_buffers_previous_page_callback",
+		},
+	},
+	{ provider = " " },
+	condition = function(self)
+		return self.shared.page > 1
+	end,
+}
+
+local function minimum_component_width(component)
+	if component.condition and not component:condition() then
+		return 0
+	end
+
+	if component.init then
+		component:init()
+	end
+
+	if rawget(component, "flexible") then
+		return minimum_component_width(component[#component])
+	elseif rawget(component, "provider") then
+		local provider = type(component.provider) == "function" and (component.provider(component) or "")
+			or (component.provider or "")
+		return heirline_utils.count_chars(provider)
+	else
+		local width = 0
+		for i = 1, #component do
+			width = width + minimum_component_width(component[i])
+		end
+		return width
+	end
+end
+
+local buffers = {
+	init = function(self)
+		self.shared = self.shared
+			or {
+				parent = self,
+				children = {},
+				page_forced = false,
+				page_count = 1,
+				page_starts = { 1 },
+				page_ends = { 1 },
+				page = 1,
+			}
+	end,
+	previous_page_button,
+	{
 		init = function(self)
 			self.buffers = vim.tbl_filter(function(buffer)
 				return vim.api.nvim_buf_is_valid(buffer) and vim.api.nvim_get_option_value("buflisted", { buf = buffer })
 			end, vim.api.nvim_list_bufs())
 
+			local children = {}
 			for index, buffer in ipairs(self.buffers) do
-				local child = self[index]
+				local child = self.shared.children[index]
 				if not (child and child.buffer == buffer) then
-					self[index] = self:new(self.buffer, index)
-					child = self[index]
+					child = self:new(buffer_template, index)
 					child.buffer = buffer
 				end
 
+				children[index] = child
+
 				if buffer == tonumber(vim.g.actual_curbuf) and vim.g.tsknvim_in_focus then
+					if not child.is_active then
+						self.shared.page_forced = false
+					end
 					child.is_active = true
 				else
 					child.is_active = false
 				end
 			end
-			if #self > #self.buffers then
-				for index = #self.buffers + 1, #self do
-					self[index] = nil
+
+			self.shared.children = children
+
+			local maximum_page_width = vim.opt.columns:get() - 4 - 4 - 3
+			local page_count = 1
+			local page_width = 0
+			local page_starts = { 1 }
+			local page_ends = {}
+			for index = 1, #self.shared.children do
+				local width = minimum_component_width(self.shared.children[index])
+				if page_width + width > maximum_page_width and page_starts[page_count] < index then
+					page_ends[page_count] = index - 1
+					page_count = page_count + 1
+					page_starts[page_count] = index
+					page_width = width
+				else
+					page_width = page_width + width
 				end
+				if not self.shared.page_forced and self.shared.children[index].is_active then
+					self.shared.page = page_count
+				end
+			end
+			page_ends[page_count] = #self.buffers
+
+			self.shared.page_count = page_count
+			self.shared.page_starts = page_starts
+			self.shared.page_ends = page_ends
+			self.shared.page = math.min(self.shared.page, page_count)
+
+			for i = self.shared.page_starts[self.shared.page], self.shared.page_ends[self.shared.page] + 1 do
+				self[i - self.shared.page_starts[self.shared.page] + 1] = self.shared.children[i]
+			end
+			for i = self.shared.page_ends[self.shared.page] - self.shared.page_starts[self.shared.page] + 2, #self do
+				self[i] = nil
 			end
 		end,
 		update = {
@@ -303,6 +453,8 @@ local buffers = {
 			bold = true,
 		},
 	},
+	{ provider = "%=" },
+	next_page_button,
 }
 
 return buffers
